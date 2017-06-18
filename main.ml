a036620d4bafb62af1f55affe16b0167693488fe
Raman Varabets <roman.vorobets@gmail.com>
2017-06-18 14:16:03 +0800

    run_workers with lwt stream interface

--- main.ml
+++ main.ml
@@ -57,51 +57,61 @@ let input_value ?on_exn input =
   match%lwt Lwt_io.read_value input with
   | exception End_of_file -> Lwt.return_none
   | exception exn ->
+    log #info ~exn "input_value";
     let%lwt () = match on_exn with Some on_exn -> on_exn exn | None -> Lwt.return_unit in
     Lwt.return_none
   | v ->
     Lwt.return_some v
-
-let input_stream ?on_exn input =
-  Lwt_stream.from (fun () -> input_value ?on_exn input)
 
 let output_value output v =
   let%lwt () = Lwt_io.write_value output v in
   let%lwt () = Lwt_io.flush output in
   Lwt.return_unit
 
+let input_stream ?on_exn input = Lwt_stream.from (fun () -> input_value ?on_exn input)
+
 let output_stream output stream = Lwt_stream.iter_s (output_value output) stream
 
 let worker (execute : task Lwt_stream.t -> result Lwt_stream.t) =
-  let main_read, child_write = Unix.pipe () in
-  let child_read, main_write = Unix.pipe () in
+  let (main_read, child_write) = Unix.pipe () in
+  let (child_read, main_write) = Unix.pipe () in
   match Nix.fork () with
-  | `Child -> (* child *)
-      Unix.close main_read; Unix.close main_write;
-      Unix.set_close_on_exec child_read;
-      Unix.set_close_on_exec child_write;
-      Lwt_main.run begin
-        let output = Lwt_io.of_unix_fd ~mode:Lwt_io.Output child_write in
-        let input = Lwt_io.of_unix_fd ~mode:Lwt_io.Input child_read in
-        let%lwt () =
-          let on_exn exn = log #error ~exn "Parallel.worker failed to unmarshal task"; Lwt.return_unit in
-          input_stream ~on_exn input |>
-          execute |>
-          output_stream output
-        in
-        let%lwt () = exn_lwt_suppress Lwt_io.close input in
-        let%lwt () = exn_lwt_suppress Lwt_io.close output in
-        Lwt.return_unit
-      end;
-      exit 0
+  | `Child ->
+    Unix.close main_read; Unix.close main_write;
+    Unix.set_close_on_exec child_read;
+    Unix.set_close_on_exec child_write;
+    Lwt_main.run begin
+      let output = Lwt_io.of_unix_fd ~mode:Lwt_io.Output child_write in
+      let input = Lwt_io.of_unix_fd ~mode:Lwt_io.Input child_read in
+      let (accept_stream, push) = Lwt_stream.create () in
+      let result_stream =
+        let on_exn exn = log #error ~exn "Parallel.worker failed to unmarshal task"; Lwt.return_unit in
+        input_stream ~on_exn input |>
+        Lwt_stream.filter (fun _ -> push (Some `Accepted); true) |>
+        execute |>
+        Lwt_stream.map (fun x -> `Result x)
+      in
+      let result_stream =
+        Lwt_stream.append result_stream @@
+        Lwt_stream.from (fun () -> push None; Lwt.return_none)
+      in
+      let%lwt () =
+        Lwt_stream.choose [ accept_stream; result_stream; ] |>
+        output_stream output
+      in
+      let%lwt () = exn_lwt_suppress Lwt_io.close input in
+      let%lwt () = exn_lwt_suppress Lwt_io.close output in
+      Lwt.return_unit
+    end;
+    exit 0
   | `Forked pid ->
-      Unix.close child_read; Unix.close child_write;
-      (* prevent sharing these pipes with other children *)
-      Unix.set_close_on_exec main_write;
-      Unix.set_close_on_exec main_read;
-      let cout = Lwt_io.of_unix_fd ~mode:Lwt_io.Output main_write in
-      let cin = Lwt_io.of_unix_fd ~mode:Lwt_io.Input main_read in
-      { ch = Some (cin, cout); pid; }
+    Unix.close child_read; Unix.close child_write;
+    (* prevent sharing these pipes with other children *)
+    Unix.set_close_on_exec main_write;
+    Unix.set_close_on_exec main_read;
+    let cout = Lwt_io.of_unix_fd ~mode:Lwt_io.Output main_write in
+    let cin = Lwt_io.of_unix_fd ~mode:Lwt_io.Input main_read in
+    { ch = Some (cin, cout); pid; }
 
 let create execute n =
   let running = List.init n (fun _ -> worker execute) in
@@ -165,7 +175,7 @@ let stop ?wait t =
   | `Killed killed -> log #info "Timeouted, killing %d (of %d) workers with SIGKILL%s" killed (List.length l) (gone ()); Lwt.return_unit
 
 let perform t ?(autoexit=false) tasks =
-  let streams =
+  let stream =
     match t.running with
     | [] -> [ t.execute tasks; ] (* no workers *)
     | _ ->
@@ -183,39 +193,39 @@ let perform t ?(autoexit=false) tasks =
         t.running <- List.filter (fun w' -> w'.pid <> w.pid) t.running;
         Lwt.return_unit
       in
-      let output =
-        let rec loop () =
-          match%lwt Lwt_mutex.with_lock lock (fun () -> Lwt_stream.get tasks) with
-          | Some x -> let%lwt () = output_value cout x in loop ()
-          | None -> Lwt.return_unit
-        in
-        loop ()
+      let output () =
+        match%lwt Lwt_mutex.with_lock lock (fun () -> Lwt_stream.get tasks) with
+        | Some x -> output_value cout x
+        | None -> exn_lwt_suppress Lwt_io.close cout
       in
-      let input =
-        let input = input_stream ~on_exn cin in
-        Lwt_stream.from begin fun () ->
-          match%lwt Lwt_stream.get input with
-          | Some _ as x -> Lwt.return x
-          | None ->
-            Lwt.cancel output;
-            Lwt.return_none
-        end
+      let start = Lwt_stream.from (fun () -> let%lwt () = output () in Lwt.return_none) in
+      let input = input_stream ~on_exn cin in
+      let rec next () =
+        match%lwt Lwt_stream.get input with
+        | Some `Accepted ->
+          let%lwt () = output () in
+          next ()
+        | Some (`Result x) ->
+          Lwt.return_some x
+        | None ->
+          let%lwt () = exn_lwt_suppress Lwt_io.close cin in
+          let%lwt () = close_ch w in
+          Lwt.return_none
       in
-      Some input
+      Some (Lwt_stream.append start (Lwt_stream.from next))
     end
   in
-  let stream = Lwt_stream.choose streams in
-  Lwt_stream.from begin fun () ->
-    match%lwt Lwt_stream.get stream with
-    | Some _ as answer -> Lwt.return answer
-    | None ->
-    begin match t.gone with
-    | 0 -> log #info "Finished"
-    | n -> log #warn "Finished, %d workers vanished" n
-    end;
-    let%lwt () = stop t in
-    Lwt.return_none
-  end
+  let finish =
+    Lwt_stream.from begin fun () ->
+      begin match t.gone with
+      | 0 -> log #info "Finished"
+      | n -> log #warn "Finished, %d workers vanished" n
+      end;
+      let%lwt () = stop t in
+      Lwt.return_none
+    end
+  in
+  Lwt_stream.append (Lwt_stream.choose stream) finish
 
 end
 
@@ -232,92 +242,34 @@ let run_workers_lwt_stream workers (type t) (type u) (f : t Lwt_stream.t -> u Lw
   Nix.handle_sig_exit_with ~exit:true (fun () -> Lwt.async (fun () -> W.stop proc)); (* FIXME: output in signal handler *)
   W.perform ~autoexit:true proc stream
 
-let run_lwt_workers workers (type t) (f : t Lwt_stream.t -> unit Lwt.t) enum =
-  let enum = Enum.append (Enum.map (fun x -> Some x) enum) (Enum.init workers (fun _ -> None)) in
-  let thread = ref None in
-  let (stream, push) = Lwt_stream.create_bounded 1 in
-  let worker event =
-    print_endline "start worker";
-    Lwt_main.run begin
-      print_endline "start lwt_main_run";
-      let%lwt () = f stream in
-      print_endline "stop lwt_main_run";
-      Lwt.return_unit
-    end;
-    print_endline "stop worker"
-  in
-  let worker = function
-    | Some x ->
-      if !thread = None then begin
-        print_endline "create thread";
-        thread := Some (Thread.create worker ());
-      end;
-      print_endline "call run_in_main";
-      Lwt_preemptive.run_in_main begin fun () ->
-        print_endline "start run_in_main";
-        let%lwt () = Lwt_unix.sleep 0.1 in
-        let%lwt () = Lwt.pick [ push #push x; Daemon.wait_exit (); ] in
-        print_endline "stop run_in_main";
-        Lwt.return_unit
-      end;
-      print_endline "called run_in_main"
-    | None ->
-      print_endline "close stream";
-      begin match !thread with
-      | None -> ()
-      | Some thread ->
-        Lwt_preemptive.run_in_main begin fun () ->
-          push #close;
-          Lwt.return_unit
-        end;
-        print_endline "join thread";
-        Thread.join thread
-      end;
-      print_endline "raise success";
-      raise Success
-  in
-  run_workers workers worker id enum
-
 let () =
-  (*
   print_endline "start 10 workers";
   run_workers 10 (printf "start %d\n") id (List.enum (List.init 100 id));
- *)
-(*
-  match Lwt_unix.fork () with
-  | 0 -> print_endline "in child"; exit 0
-  | _ ->
-*)
-  print_endline "create enum";
-  let _enum = Enum.append (List.enum [ "first line" ]) (Enum.init 100 (sprintf "line %d")) in
-  let stream =
-    Lwt_stream.append (Lwt_stream.of_list [ "first line"; ]) (Lwt_stream.of_list (List.init 100 (sprintf "line %d")))
-  in
-  print_endline "start 1 worker";
-(*
-  run_workers 1 print_endline id enum;
-*)
-(*
-  run_lwt_workers 1 begin fun stream ->
-    print_endline "start lwt worker";
-    let%lwt () = Lwt_stream.iter print_endline stream in
-    print_endline "stop lwt worker";
-    Lwt.return_unit
-  end enum
-*)
-  Lwt_main.run begin
+  Lwt_main.run (Lwt_main.yield ());
+  let test_workers nr =
     let stream =
-      run_workers_lwt_stream 1 begin fun stream ->
-        print_endline "start lwt worker";
-        let stream =
-          Lwt_stream.map begin fun line ->
-            print_endline line;
-            sprintf "return %s" line
-          end stream
-        in
-        print_endline "stop lwt worker";
-        stream
-      end stream
+      Lwt_stream.append (Lwt_stream.of_list [ "first line"; ]) (Lwt_stream.of_list (List.init 100 (sprintf "line %d")))
     in
-    Lwt_stream.iter print_endline stream
+    printf "start %d workers\n" nr;
+    run_workers_lwt_stream nr begin fun stream ->
+      print_endline "start lwt worker";
+      let stream =
+        Lwt_stream.map_s begin fun line ->
+          let%lwt () = Lwt_unix.sleep 1. in
+          let pid = Unix.getpid () in
+          printf "%s pid %d\n" line pid;
+          flush stdout;
+          Lwt.return (sprintf "return %s from pid %d" line pid)
+        end stream
+      in
+      print_endline "stop lwt worker";
+      stream
+    end stream |>
+    Lwt_stream.iter print_endline
+  in
+  Lwt_main.run begin
+    let%lwt () = test_workers 10 in
+    let%lwt () = Lwt_main.yield () in
+    let%lwt () = test_workers 20 in
+    Lwt.return_unit
   end
